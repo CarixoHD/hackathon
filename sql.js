eca9ec501bee1eb4a633fb4a11e6d5430a151eea
Qerbz <patrickfalkeid@gmail.com>
2021-10-30 19:26:52 +0200

    JASON BOURNE

--- sql.js
+++ sql.js
@@ -1,24 +1,23 @@
 var mysql = require('mysql')
+
 var con = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "Climate_Denier1337",
     database: "gimep"
-  });
-  
-  con.connect(function(err) {
+});
+      
+con.connect(function(err) {
     if (err) throw err;
     console.log("Connected")
-  });
-  
+});
 
 function doSQL(sql)
 {
     con.query(sql,function (err,result){
         if (err) throw err;
-        console.log("1 record inserted");
         return result;
-    })
+    });
 }
 
 function makeUser(username,password,team)
@@ -35,13 +34,13 @@ function makeTeam(name,pm)
 
 function makeTask(name,user)
 {
-    let sql = "INSERT INTO task(name,user, status) VALUES (\""+name+"\","+user+", 1)";
+    let sql = "INSERT INTO task(name,user, status) VALUES (\""+name+"\","+user+", 0)";
     doSQL(sql);
 }
 
 function makeMilestone(team,reward,tp_needed,name)
 {
-    let sql = "INSERT INTO milestone(team,reward,tp_needed,name) VALUES ("+team+",\""+reward+"\","+tp_needed+",\""+name+"\")";
+    let sql = "INSERT INTO milestone(team,reward,tp_needed,name,status) VALUES ("+team+",\""+reward+"\","+tp_needed+",\""+name+"\",0)";
     doSQL(sql);
 }
 
@@ -51,19 +50,37 @@ function selectAllFrom(table) {
     return doSQL(sql);
 }
 
-function selectSpecificFrom(table, field, value) {
-    let sql = `SELECT ${field} FROM ${table} WHERE ${field}=${value}`;
+function selectSpecificFrom(table, field, value, selectField) {
+    let sql = `SELECT ${field} FROM ${table} WHERE ${selectField}=${value}`;
     return doSQL(sql);
+}
 
+function milestoneProgress(milestone,userid)
+{
+    let milestonetp = sql.selectSpecificFrom("milestone","tp_needed",milestone,"id");
+    let tasksDoneByTeam = sql.doSQL(```
+    SELECT COUNT(task.id), user.id, user.name, team.id, team.name
+    FROM task 
+    INNER JOIN user, team 
+    WHERE user.team = team.id 
+    AND task.user = user.id 
+    AND task.status = 1 
+    AND user.id = ${userid};```);
+    console.log(milestonetp,tasksDoneByTeam);
+    progress = milestonetp/tasksDoneByTeam;
+    return progress;
 }
 
 
 
 var sql = {
+    con: con,
     doSQL: doSQL,
     makeUser: makeUser,
     makeTeam: makeTeam,
     makeTask: makeTask,
-    makeMilestone: makeMilestone
+    makeMilestone: makeMilestone,
+    selectSpecificFrom: selectSpecificFrom,
+    msPro: milestoneProgress
 }
 module.exports = sql
